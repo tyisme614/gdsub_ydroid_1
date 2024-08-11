d334b194149ea06bb2842a4131ab7f4a3da7f290
Yuan <yuant614@gmail.com>
2024-08-11 20:09:38 +0800

    v0.0.2 added redis for caching report and video info

--- channelWorker.js
+++ channelWorker.js
@@ -5,29 +5,34 @@ const youtube = google.youtube('v3');
 const fs = require('fs');
 const EventEmitter = require('node:events');
 
-//constants
-let TOTAL_VIDEOS = 10;
+//analysis parameters
+let TOTAL_VIDEOS = 32;
 let CHANNEL = '';
+let CHANNEL_ID = '';
+let TOP_COUNT = 10;
 //events
 const EVENT_INITIALIZED = 1000;
 const EVENT_REQUEST_VIDEO_INFO = 1001;
 const EVENT_RANK_VIDEOS = 1002;
 const EVENT_CALCULATE_COMPLETE = 1003;
+const EVENT_CACHE_DATA_IN_REDIS = 1004;
+
+const REDIS_KEY_TOP_VIEWS_LIST = 'top.views.list';
+const REDIS_KEY_TOP_LIKES_LIST = 'top.likes.list';
+const REDIS_KEY_TOP_COMMENTS_LIST = 'top.comments.list';
+const REDIS_KEY_TOP_VIDEOS_LIST = 'top.videos.list';
 
 const handler = new EventEmitter();
 //local variables
+let analyzing = false;
 let auth_key = __dirname + '/auth_key.json';
 let YOUTUBE_API_KEY, GMAIL_API_KEY;
 let videos = [];
 let video_map;
 let view_map, like_map, favorite_map, comment_map, score_map;
 let top_videos, top_views, top_likes, top_comments;
-
-
-
-
-
-
+let callback = null;
+let redis = null;
 /**
  *
  *
@@ -46,32 +51,63 @@ handler.on(EVENT_INITIALIZED, ()=>{
     top_views = [];
     top_likes = [];
     top_comments = [];
-
+    if(callback != null){
+        callback(EVENT_INITIALIZED);
+    }
 });
 handler.on(EVENT_RANK_VIDEOS, ()=>{
     calculateStatistics();
 });
 
 handler.on(EVENT_CALCULATE_COMPLETE, ()=>{
-    generateReport();
+    analyzing = false;
+    let report = generateReport();
+    handler.emit(EVENT_CACHE_DATA_IN_REDIS, CHANNEL, report);
+    // generateReport();
+    if(callback != null){
+        callback(EVENT_CALCULATE_COMPLETE);
+    }
 });
 
-// handler.on(EVENT_REQUEST_VIDEO_INFO, (videoid) =>{
-//
-// });
+handler.on(EVENT_CACHE_DATA_IN_REDIS, (key, value) => {
+    if( redis != null){
+        redis.set(key, value);
+    }else{
+        console.log('redis is not initialized...');
+    }
+
+});
+
 
 /**
  *
  * public functions
  *
  */
+
+/**
+ *
+ *
+ * @param _callback
+ *      [prototype] function(event:number)
+ *      notify the main thread that the processing is complete.
+ */
 function process(){
-    retrieveVideoList(CHANNEL, 'Trailer', TOTAL_VIDEOS);
+    analyzing = true;
+    retrieveVideoList(CHANNEL_ID, 'Official Trailer', TOTAL_VIDEOS);
+
 }
 
-function initialize(_auth, _channel, _total){
+function isAnalyzing(){
+    return analyzing;
+}
+
+function initialize(_auth, _redis, _channel, _channel_id, _total, _callback){
+    callback = _callback;
     CHANNEL = _channel;
+    CHANNEL_ID = _channel_id;
     TOTAL_VIDEOS = _total;
+    redis = _redis;
     fs.readFile(_auth, function(err, data){
         if(err) {
             console.log(err);
@@ -79,10 +115,10 @@ function initialize(_auth, _channel, _total){
         let raw = JSON.parse(data);
         YOUTUBE_API_KEY = raw.youtube_apikey;
         GMAIL_API_KEY = raw.gmail_apikey;
-
         handler.emit(EVENT_INITIALIZED);
 
     });
+
 }
 /**
  *
@@ -130,6 +166,7 @@ function retrieveVideoInfo(id){
      * title : string
      * description : string
      * publish_time : string
+     * update_time : string
      * statistics : object
      * score : number
      * rank_v : number
@@ -170,6 +207,7 @@ function retrieveVideoInfo(id){
                     title : _title,
                     description : _description,
                     publish_time : _publish_time,
+                    update_time : new Date(),
                     statistics : {
                         view_count : _view_count,
                         like_count : _like_count,
@@ -194,9 +232,10 @@ function retrieveVideoInfo(id){
                 comment_map.set(_comment_count, _video_id);
                 if(videos.length >= TOTAL_VIDEOS){
                     handler.emit(EVENT_RANK_VIDEOS);
-                }else{
-                    console.log('received video count -->' + videos.length);
                 }
+                // else{
+                //     console.log('received video count -->' + videos.length);
+                // }
             }
 
 
@@ -215,10 +254,13 @@ function calculateStatistics(){
         v.rank_v = rank + 1;
         rank++;
         // console.log('Video:' + v.title + ' BASE_V=' + v.BASE_V);
-        if(top_views.length < 3){
+        if(top_views.length < TOP_COUNT){
             top_views.push(v);
         }
     }
+
+    //cache top views list
+    handler.emit(EVENT_CACHE_DATA_IN_REDIS, REDIS_KEY_TOP_VIEWS_LIST, JSON.stringify(top_views));
 
     let sorted_lc = new Map([...like_map.entries()].sort((a, b) => b[0] - a[0]));
     rank = 0;
@@ -228,11 +270,13 @@ function calculateStatistics(){
         v.BASE_L = TOTAL_VIDEOS - rank;
         v.rank_l = rank + 1;
         rank++;
-        if(top_likes.length < 3){
+        if(top_likes.length < TOP_COUNT){
             top_likes.push(v);
         }
         // console.log('Video:' + v.title + ' BASE_L=' + v.BASE_L);
     }
+    //cache top likes list
+    handler.emit(EVENT_CACHE_DATA_IN_REDIS, REDIS_KEY_TOP_LIKES_LIST, JSON.stringify(top_likes));
 
     let sorted_cc = new Map([...comment_map.entries()].sort((a, b) => b[0] - a[0]));
     rank = 0;
@@ -242,12 +286,14 @@ function calculateStatistics(){
         v.BASE_C = TOTAL_VIDEOS - rank;
         v.rank_c = rank + 1;
         rank++;
-        if(top_comments.length < 3){
+        if(top_comments.length < TOP_COUNT){
             top_comments.push(v);
         }
 
         // console.log('Video:' + v.title + ' BASE_C=' + v.BASE_C);
     }
+    //cache top comments list
+    handler.emit(EVENT_CACHE_DATA_IN_REDIS, REDIS_KEY_TOP_COMMENTS_LIST, JSON.stringify(top_comments));
 
     for(let[key, value] of video_map){
         let v = value;
@@ -262,41 +308,63 @@ function calculateStatistics(){
         let v = value;
         v.rank_total = rank + 1;
         rank++;
-        if(top_videos.length < 3){
+        if(top_videos.length < TOP_COUNT){
             top_videos.push(v);
         }
+        handler.emit(EVENT_CACHE_DATA_IN_REDIS, v.video_id, JSON.stringify(v));
         // console.log('Video:' + v.title + ' BASE_C=' + v.BASE_C);
     }
+    //cache top videos list
+    handler.emit(EVENT_CACHE_DATA_IN_REDIS, REDIS_KEY_TOP_VIDEOS_LIST, JSON.stringify(top_videos));
 
     handler.emit(EVENT_CALCULATE_COMPLETE);
 
 }
 
 function generateReport(){
-    console.log('\nTop Views' + '\n');
+    let report = '';
+    report += '\n[Top Views]' + '\n'
+
     for(let i=0; i<top_views.length; i++){
         let v = top_views[i];
-        console.log('No.' + (i+1) + ' Video:' + v.title + '\nTotal Views -->' + v.statistics.view_count);
+        report += 'No.' + (i+1) + ' Video:' + v.title + '\nTotal Views -->' + v.statistics.view_count + '\n';
+
     }
 
-    console.log('\nTop Likes' + '\n');
+    report += '\n[Top Likes]' + '\n';
     for(let i=0; i<top_likes.length; i++){
         let v = top_likes[i];
-        console.log('No.' + (i+1) + ' Video:' + v.title + '\nTotal Likes -->' + v.statistics.like_count);
+        report += 'No.' + (i+1) + ' Video:' + v.title + '\nTotal Likes -->' + v.statistics.like_count + '\n';
+
     }
 
-    console.log('\nTop Comments' + '\n');
+    report += '\n[Top Comments]' + '\n';
     for(let i=0; i<top_comments.length; i++){
         let v = top_comments[i];
-        console.log('No.' + (i+1) + ' Video:'  + v.title + '\nTotal Comments -->' + v.statistics.comment_count);
+        report += 'No.' + (i+1) + ' Video:'  + v.title + '\nTotal Comments -->' + v.statistics.comment_count + '\n';
     }
 
-    console.log('\nVideo Rank' + '\n');
+    report += '\n[Video Rank]' + '\n';
+
     for(let i=0; i<top_videos.length; i++){
         let v = top_videos[i];
-        console.log('No.' + (i+1) + ' Video:' + v.title + '\nTotal Score -->' + v.score);
-        displayVideoInfo(v);
+        report += 'No.' + (i+1) + ' Video:' + v.title + '\nTotal Score -->' + v.score + '\n';
+        report += getVideoInfoText(v) + '\n';
     }
+
+    return report;
+}
+
+function getVideoInfoText(v){
+    let message = '\n[VIDEO INFO]' + '\n'
+        + 'Video Title:' + v.title  + '\n'
+        + 'Video ID:' + v.video_id  + '\n'
+        + 'Video Score:' + v.score  + '\n'
+        + 'Views Count Rank:' + v.BASE_V  + '\n'
+        + 'Likes Count Rank:' + v.BASE_L  + '\n'
+        + 'Comments Count Rank:' + v.BASE_C  + '\n';
+    return message;
+
 }
 
 function displayVideoInfo(v){
@@ -313,7 +381,8 @@ function displayVideoInfo(v){
 }
 
 //exports
-module.exports = {initialize, process}
+module.exports = {initialize, process, isAnalyzing, generateReport, getVideoInfoText,
+    EVENT_CALCULATE_COMPLETE, EVENT_INITIALIZED};
 
 
 /***
--- workerMgr.js
+++ workerMgr.js
@@ -2,17 +2,58 @@ const { argv } = require('node:process');
 const channelWorker = require('./channelWorker');
 
 const AUTH_KEY = __dirname + '/auth_key.json';
-const CHECK_INTERVAL = 3600000; //1 hour
+const CHECK_INTERVAL = 3600000; //24 hours
+const redisClient = require('redis');
+
+let redis;
 // const CHANNEL_NETFLIX = 'UCWOA1ZGywLbqmigxE4Qlvuw';
 //const CHANNEL_DISNEY = 'UCuaFvcY4MhZY3U43mMt1dYQ';
 //const CHANNEL_HBO = 'UCVTQuK2CaWaTgSsoNkn5AiQ';
 //const CHANNEL_UNIVERSAL = 'UCq0OueAsdxH6b8nyAspwViw';
 
-channelWorker.initialize(AUTH_KEY, argv[2], 32);
+initializeRedis().then(r => {
+    redis = r;
 
-let worker = setInterval(doProcess, CHECK_INTERVAL );
+    //!!!NOTE: specify target channel in argv[2], candidates: netflix, disney, hbo, universal
+    //this is also used as report key in redis
+    channelWorker.initialize(AUTH_KEY, redis, argv[2], argv[3], 32, (event) => {
+        switch(event){
+            case channelWorker.EVENT_INITIALIZED:
+                doProcess();
+                // let worker = setInterval(doProcess, CHECK_INTERVAL );
+                break;
+            case channelWorker.EVENT_CALCULATE_COMPLETE:
+                console.log(currentTimestamp() + '[workerMgr]' + '[' + argv[2] + '] analysis complete.');
+                break;
+        }
+    });
+});
 
+
+
+
+async function initializeRedis(){
+      const _redis = await redisClient.createClient()
+        .on('error', err => console.log('Redis Client Error', err))
+        .connect();
+
+     return _redis;
+}
 function doProcess(){
     channelWorker.process();
 }
 
+function currentTimestamp(){
+    let ts = Date.now();
+
+    let date_ob = new Date(ts);
+    let date = date_ob.getDate();
+    let month = date_ob.getMonth() + 1;
+    let year = date_ob.getFullYear();
+
+    let hour = date_ob.getHours();
+    let minute = date_ob.getMinutes();
+    let second = date_ob.getSeconds();
+
+    return '[' + year + "-" + month + "-" + date + '_' + hour + ':' + minute +':' + second + ']';
+}
